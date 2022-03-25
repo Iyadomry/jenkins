42a994985c6fa1182a4d42b30f7076464026c2f0
Iyadomry <iyad2fast@hotmail.com>
2022-03-25 16:21:00 +0300

    Update mutli_fun.groovy

--- scripted/mutli_fun.groovy
+++ scripted/mutli_fun.groovy
@@ -1,10 +1,12 @@
- node('jenkins') {
+def first() {
+    node('jenkins') {
         stage('clone git repo') {
             echo ' stage of git'
         }
         next()
     }
- 
+}
+
 def next() {
     echo 'test 2'
 }
