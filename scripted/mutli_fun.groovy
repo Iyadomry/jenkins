4bdfcc988d64a6cb7abf11a0c710c36bbe1f84f4
Iyadomry <iyad2fast@hotmail.com>
2022-03-25 23:24:46 +0300

    Update mutli_fun.groovy

--- scripted/mutli_fun.groovy
+++ scripted/mutli_fun.groovy
@@ -18,7 +18,7 @@ def first() {
 
 def sendmail() {
     node('jenkins') {
-        mail bcc: '', body: "This is build number '${BUILD_NUMBER}'", cc: '', from: '', replyTo: '', subject: 'JenkinsReport', to: 'iyad.omryx@gmail.com'
+        mail bcc: '', body: "This is build number '${BUILD_NUMBER}'", cc: '', from: '', replyTo: '', subject: "${BUILD_DISPLAY_NAME}", to: 'iyad.omryx@gmail.com'
     }
 }
 
