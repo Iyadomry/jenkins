2da9fc13b7d9748febce17792bbc90e38e0ec656
Iyadomry <iyad2fast@hotmail.com>
2022-03-27 19:40:11 +0300

    Update if_statment.groovy

--- scripted/if_statment/if_statment.groovy
+++ scripted/if_statment/if_statment.groovy
@@ -6,15 +6,12 @@ def start() {
     node('jenkins') {
         stage(' running if statment') {
             def $VERSION = version
-            switch("${version}") {
-                case "1":
-                    "${version}" !=  "windows".toString() 
-                    println (" we get the manual inout")
-                case  "2" :
-                    "${version}" ==  "windows".toString() 
-                    println (" this the default value and it should be Windows ")
-                break;
+            if ( version != "windows") {
+                echo " This is the manula value "
             }
+            else {
+                echo " this is the default value 'windows' "
+            } 
         }
     }
 }
