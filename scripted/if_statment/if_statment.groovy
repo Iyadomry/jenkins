b86f1c9283d1b0ce4f74cfd4f8fcc0c7dd90cb18
Iyadomry <iyad2fast@hotmail.com>
2022-03-27 18:56:58 +0300

    Update if_statment.groovy

--- scripted/if_statment/if_statment.groovy
+++ scripted/if_statment/if_statment.groovy
@@ -4,8 +4,16 @@ def main() {
 
 def start() {
     node('jenkins') {
+        ${env.VERSION} = version
         stage(' running if statment') {
             echo ' hello there'
+            switch(version) {
+                case 100:
+                    println("this is the default value")
+                    
+                break
+                    println("this is the default value")
+            }
         }
     }
 }
