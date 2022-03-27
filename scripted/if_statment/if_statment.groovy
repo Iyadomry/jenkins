a3ccd93d03d22ac138ee835908adfbd7f7f66da7
Iyadomry <iyad2fast@hotmail.com>
2022-03-27 19:08:10 +0300

    Update if_statment.groovy

--- scripted/if_statment/if_statment.groovy
+++ scripted/if_statment/if_statment.groovy
@@ -8,7 +8,7 @@ def start() {
         stage(' running if statment') {
             echo ' hello there'
             switch(version) {
-                case [ version != int 100 ] :
+                case [ version !=  "100" ] :
                     println (" we get the manual inout")
                 default:
                     println (" this the default value and it should be 100 ")
