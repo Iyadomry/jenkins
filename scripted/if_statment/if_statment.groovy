12b534b0a713613ab14b74cb2de2b2a056aced8a
Iyadomry <iyad2fast@hotmail.com>
2022-03-27 19:41:15 +0300

    Update if_statment.groovy

--- scripted/if_statment/if_statment.groovy
+++ scripted/if_statment/if_statment.groovy
@@ -7,7 +7,7 @@ def start() {
         stage(' running if statment') {
             def $VERSION = version
             if ( version != "windows") {
-                echo " This is the manula value "
+                echo " This is the manula value ${version} "
             }
             else {
                 echo " this is the default value 'windows' "
