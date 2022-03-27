3225eeda61b1172a8a851b330f1897855b77b287
Iyadomry <iyad2fast@hotmail.com>
2022-03-27 19:27:17 +0300

    Update if_statment.groovy

--- scripted/if_statment/if_statment.groovy
+++ scripted/if_statment/if_statment.groovy
@@ -8,9 +8,11 @@ def start() {
         stage(' running if statment') {
             echo "${version}"
             switch("${version}") {
-                case [ "${version}" !=  "windows".toString() ] :
+                case "1":
+                    "${version}" !=  "windows".toString() 
                     println (" we get the manual inout")
-                case [ "${version}" ==  "windows".toString() ]:
+                case  "2" :
+                    "${version}" ==  "windows".toString() 
                     println (" this the default value and it should be Windows ")
                 break;
             }
